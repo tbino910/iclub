687d4d45661b8955c22977ba147902280f54282b
tbino <tbino910@gmail.com>
2015-01-15 01:15:46 +0900

    코드이크나이터 기본 셋�

---
+++ application/controllers/main.php
@@ -0,0 +1,18 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Main extends CI_Controller {
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		$this->main();
+	}
+
+	public function main()
+	{
+		$this->load->view('main_v');
+	}
+}
+?>
---
+++ application/views/main_v.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>믿음이 가는 컴퓨터 쇼핑! 아이클럽</title>
+	</head>
+	<body>
+		<div id="header"></div>
+		<div id="section">
+			<div id="left-wing"> </div>
+			<div id="continer"></div>
+			<div id="right-wing"></div>
+		</div>
+		<div id="footer"></div>
+	</body>
+</html>
